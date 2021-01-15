81734248edefc149939ea03d897ac6f00c3d63bb
Karol Witkowski <karol.witkowski@hotmail.com>
2021-01-15 18:19:40 +0100

    Tools badge update
    Remove vsc and gitkraken badge, add vuetify

--- client/src/components/chat/Chat.vue
+++ client/src/components/chat/Chat.vue
@@ -1,7 +1,17 @@
 <template>
-  <section>
-    <h1>There will be a chatbox soon...</h1>
-  </section>
+ <v-container fluid style="padding: 0;">
+    <v-row no-gutters>
+      <v-col sm="2" class="scrollable">
+      </v-col>
+      <v-col sm="10" style="position: relative;">
+        <div class="chat-container"  ref="chatContainer" >
+        </div>
+        <div class="typer">
+          <input type="text" placeholder="Type message">
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
