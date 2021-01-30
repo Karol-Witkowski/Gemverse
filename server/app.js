72b963396b9c7a96b9f6a1e479edac8337273e15
Karol Witkowski <karol.witkowski@hotmail.com>
2021-01-30 22:00:13 +0100

    Add database folder
    Move db config to separate folder

--- server/app.js
+++ server/app.js
@@ -1,26 +1,9 @@
 /** Express */
 const express = require('express');
-const port = process.env.PORT || "3000";
-const app = express();
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const helmet = require('helmet');
-const server = require('http').createServer(app);
 
-/** Socket IO */
-const io = require('socket.io');
-const http = require('http');
-
-/** Addons */
-const chalk = require('chalk');
-
-/** app setup */
-app.use(cors());
-app.use(helmet());
-
-app.get('/', (req, res) => {
-  res.send('Express test');
-})
-
-server.listen(port, () => {
-  console.log(chalk.bgGreen.bold(`Gemverse backend listening at http://localhost:${port}`));
-})
+/** Connect to MongoDB */
+const mongoose = require('mongoose');
+require('./db/mongoose');
---
+++ server/db/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+mongoose.Promise = require('bluebird');
+
+const dbConnect = () => {
+  mongoose.connect(
+    process.env.DATABASE_URL,
+    { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>  console.log(chalk.green('Connection with MongoDB established')))
+  .catch((err) => console.error(err));
+}
+
+dbConnect();
+
+module.exports = {mongoose, dbConnect};
