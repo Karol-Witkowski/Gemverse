a4d37648845cbe6fe8d695e4cefe3aadb84b4a49
Karol Witkowski <karol.witkowski@hotmail.com>
2021-04-18 08:05:34 +0200

    Setup authentication controller
    Setup controller for authentication  route

---
+++ server/controllers/authenticationController.js
@@ -0,0 +1,81 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const signUp = async (req, res) => {
+  const emailDB = await User.findOne({ email : req.body.email });
+  const usernameDB = await User.findOne({ username :  { $regex : new RegExp(req.body.username, 'i') } });
+  let email = '';
+  let username = '';
+
+  if (emailDB || usernameDB) {
+    if (emailDB !== null) {
+      email =`${ req.body.email } address is already taken`;
+    }
+    if (usernameDB !== null) {
+      username = `${ req.body.username } is already taken`;
+    }
+    res.status(403).send({ email, username });
+  } else {
+    const establishUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    establishUser.save().then((UserCollection) => {
+      const user = UserCollection.toObject();
+      const token = jwt.sign(
+        user,
+        process.env.JWT_KEY,
+        { expiresIn: 24000 },
+      );
+
+      res.status(201).send({
+        auth: true,
+        success: true,
+        token: `Bearer ${ token }`,
+        user
+      });
+    })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+};
+
+const signIn = async (req, res) => {
+  const user = await User.findOne({ email : req.body.email });
+
+  if (!user) {
+    return res.status(404).json({ user: 'User not found - Try again' });
+  } else {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const token = jwt.sign(
+        user.toObject(),
+        process.env.JWT_KEY,
+        { expiresIn: 24000 }
+      );
+      await user.save();
+      return res.status(200).send({ auth: true, token: `Bearer ${ token }`, user });
+    }
+    return res.status(404).json({ password: 'Invalid password' });
+  }
+};
+
+const logoutUser = async (req, res) => {
+  const user = await User.findOne({ username: req.body.username }).select('-password');
+
+  if (!user) {
+    return res.status(404).send({ error: `${ req.body.username } not found` });
+  } else {
+    return res.status(200).send({ success: true });
+  }
+};
+
+
+module.exports = {
+  signUp,
+  signIn,
+  logoutUser
+};
--- server/routes/router/authentication.js
+++ server/routes/router/authentication.js
@@ -1,81 +1,17 @@
-const bcrypt = require('bcrypt');
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const router = express.Router();
-const User = require('../../models/User');
+const router = require('express').Router();
+const {
+  signUp,
+  signIn,
+  logoutUser
+} = require('../../controllers/authenticationController');
 
 /** Save user */
-router.post('/register', async (request, response) => {
-  const emailDB = await User.findOne({ email : request.body.email });
-  const usernameDB = await User.findOne({ username :  { $regex : new RegExp(request.body.username, 'i') } });
-  let email = '';
-  let username = '';
-
-  if (emailDB || usernameDB) {
-    if (emailDB !== null) {
-      email =`${ request.body.email } address is already taken`;
-    }
-    if (usernameDB !== null) {
-      username = `${ request.body.username } is already taken`;
-    }
-    response.status(403).send({ email, username });
-  } else {
-    const establishUser = new User({
-      username: request.body.username,
-      email: request.body.email,
-      password: request.body.password,
-    });
-
-    establishUser.save().then((UserCollection) => {
-      const user = UserCollection.toObject();
-      const token = jwt.sign(
-        user,
-        process.env.JWT_KEY,
-        { expiresIn: 24000 },
-      );
-
-      response.status(201).send({
-        auth: true,
-        success: true,
-        token: `Bearer ${ token }`,
-        user
-      });
-    })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-});
+router.post('/register', signUp);
 
 /** Login user */
-router.post('/login', async (request, response) => {
-  const user = await User.findOne({ email : request.body.email });
-
-  if (!user) {
-    return response.status(404).json({ user: 'User not found - Try again' });
-  } else {
-    if (await bcrypt.compare(request.body.password, user.password)) {
-      const token = jwt.sign(
-        user.toObject(),
-        process.env.JWT_KEY,
-        { expiresIn: 24000 }
-      );
-      await user.save();
-      return response.status(200).send({ auth: true, token: `Bearer ${ token }`, user });
-    }
-    return response.status(404).json({ password: 'Invalid password' });
-  }
-});
+router.post('/login', signIn);
 
 /** Logout user */
-router.post('/logout', async (request, response) => {
-  const user = await User.findOne({ username: request.body.username }).select('-password');
-
-  if (!user) {
-    return response.status(404).send({ error: `${ request.body.username } not found` });
-  } else {
-    return response.status(200).send({ success: true });
-  }
-});
+router.post('/logout', logoutUser);
 
 module.exports = router;
