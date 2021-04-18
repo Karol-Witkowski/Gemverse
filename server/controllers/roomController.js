5c7a69cfe3f2d7fd36f6d7f33455f8783950e477
Karol Witkowski <karol.witkowski@hotmail.com>
2021-04-18 07:12:30 +0200

    Setup room controller
    Setup controller for room route

---
+++ server/controllers/roomController.js
@@ -0,0 +1,102 @@
+const bcrypt = require('bcrypt');
+const Message = require('../models/Message');
+const Room = require('../models/Room');
+
+const getAllRooms = async (req, res) => {
+  const rooms = await Room.find().select('-password');
+
+  if (rooms.length < 1) {
+    return res.status(404).json({ error: 'Rooms not found' });
+  } else {
+    return res.status(200).json(rooms);
+  };
+};
+
+const getRoomBySlug = async (req, res) => {
+  const room = await Room.findOne({ slug: req.params.slug }).select('-password');
+
+  if (!room) {
+    return res.status(404).json({ error: 'Room not found' });
+  } else {
+    return res.status(200).json(room);
+  };
+};
+
+const postRoom = async (req, res) => {
+  const room = await Room.findOne({ name: { $regex : new RegExp(req.body.name, 'i') } })
+    .select('-password');
+
+  if (room !== null) {
+    return res.status(403).json({ error: `Name ${ req.body.name } is already taken` });
+  } else {
+    req.body.access = req.body.password ? 'private' : 'public',
+    Room.create(req.body, (error, room) => {
+      if (error) {
+        return res.status(403).json({ error: `Name ${ req.body.name } is already taken` });
+      } else res.status(201).send(room);
+    });
+  }
+};
+
+const verifyPassword = async (req, res) => {
+  const room = await Room.findOne({ name: req.body.name });
+
+  if (!room) {
+    return res.status(404).json({ error: `No room with name ${ req.body.name } found` });
+  } else {
+    if (await bcrypt.compare(req.body.password, room.password)) {
+      if (!room.permission.includes(req.user.id)) {
+        room.permission.push(req.user.id);
+      }
+      await room.save();
+      return res.status(200).send(room);
+    } else {
+      return res.status(404).json({ error: 'Invalid password' });
+    }
+  }
+};
+
+const deleteRoomById = async (req, res) => {
+  const room = await Room.findById({ _id: req.params.id });
+    if (!room) {
+      return res.status(404).json({ error: `Room not found` });
+    } else {
+      if (req.body._id === room.user.toString()) { // move it to validation?
+        await Message.deleteMany({ room: req.params.id });
+        await room.delete();
+        return res.status(200).json({ message: 'Room deleted'});
+      } else {
+        return res.status(404).json({ error: 'Users are allowed to delete only own rooms' });
+      }
+    }
+};
+
+const setUserOffline = async (req, res) => {
+  const room = await Room.findOne({ _id: req.body.id });
+
+  if (!room) {
+    return res.status(404).json({ error: `No room with id ${ req.body.id } found` });
+  } else {
+    if (room.activeUsers.find((user) => user.lookup.toString() === req.user.id)) {
+      room.activeUsers = room.activeUsers.filter((user) => user.lookup.toString() !== req.user.id);
+      if (room.permission.indexOf(req.user.id) >= 0) {
+        return room.permission.splice(room.permission.indexOf(req.user.id), 1);
+      }
+      await room.save();
+    }
+
+    res.status(200).json(await Room.populate(room, {
+      path: 'user activeUsers.lookup',
+      select: 'username'
+    }));
+  }
+};
+
+module.exports = {
+  getAllRooms,
+  getRoomBySlug,
+  postRoom,
+  verifyPassword,
+  deleteRoomById,
+  setUserOffline
+};
--- server/routes/router/room.js
+++ server/routes/router/room.js
@@ -1,104 +1,30 @@
-const bcrypt = require('bcrypt');
-const express = require('express');
+const router = require('express').Router();
 const passport = require('passport');
-const Message = require('../../models/Message');
-const Room = require('../../models/Room');
-const router = express.Router();
+const {
+  getAllRooms,
+  getRoomBySlug,
+  postRoom,
+  verifyPassword,
+  deleteRoomById,
+  setUserOffline
+} = require('../../controllers/roomController');
 
 /** Get all rooms */
-router.get('/', passport.authenticate('jwt', { session: false }), async (request, response) => {
-  const rooms = await Room.find().select('-password');
-
-  if (rooms.length < 1) {
-    return response.status(404).json({ error: 'Rooms not found' });
-  } else {
-    return response.status(200).json(rooms);
-  };
-});
+router.get('/', passport.authenticate('jwt', { session: false }), getAllRooms);
 
 /** Get single room by slug */
-router.get('/:slug', passport.authenticate('jwt', { session: false }), async (request, response) => {
-  const room = await Room.findOne({ slug: request.params.slug }).select('-password');
-
-  if (!room) {
-    return response.status(404).json({ error: 'Room not found' });
-  } else {
-    return response.status(200).json(room);
-  };
-});
+router.get('/:slug', passport.authenticate('jwt', { session: false }), getRoomBySlug);
 
 /** Save room */
-router.post('/', passport.authenticate('jwt', { session: false }), async (request, response) => {
-  const room = await Room.findOne({ name: { $regex : new RegExp(request.body.name, 'i') } })
-    .select('-password');
-
-  if (room !== null) {
-    return response.status(403).json({ error: `Name ${ request.body.name } is already taken` });
-  } else {
-    request.body.access = request.body.password ? 'private' : 'public',
-    Room.create(request.body, (error, room) => {
-      if (error) {
-        return response.status(403).json({ error: `Name ${ request.body.name } is already taken` });
-      } else response.status(201).send(room);
-    });
-  }
-});
+router.post('/', passport.authenticate('jwt', { session: false }), postRoom);
 
 /** Password verification */
-router.post('/verification', passport.authenticate('jwt', { session: false }), async (request, response) => {
-  const room = await Room.findOne({ name: request.body.name });
-
-  if (!room) {
-    return response.status(404).json({ error: `No room with name ${ request.body.name } found` });
-  } else {
-    if (await bcrypt.compare(request.body.password, room.password)) {
-      if (!room.permission.includes(request.user.id)) {
-        room.permission.push(request.user.id);
-      }
-      await room.save();
-      return response.status(200).send(room);
-    } else {
-      return response.status(404).json({ error: 'Invalid password' });
-    }
-  }
-});
+router.post('/verification', passport.authenticate('jwt', { session: false }), verifyPassword);
 
 /** Delete room */
-router.delete('/:id', passport.authenticate('jwt', { session: false }), async (request, response) => {
-  const room = await Room.findById({ _id: request.params.id });
-    if (!room) {
-      return response.status(404).json({ error: `Room not found` });
-    } else {
-      if (request.body._id === room.user.toString()) { // move it to validation?
-        await Message.deleteMany({ room: request.params.id });
-        await room.delete();
-        return response.status(200).json({ message: 'Room deleted'});
-      } else {
-        return response.status(404).json({ error: 'Users are allowed to delete only own rooms' });
-      }
-    }
-});
+router.delete('/:id', passport.authenticate('jwt', { session: false }), deleteRoomById);
 
 /** Remove user on room leave event */
-router.post('/remove/user', passport.authenticate('jwt', { session: false }), async (request, response) => {
-  const room = await Room.findOne({ _id: request.body.id });
-
-  if (!room) {
-    return response.status(404).json({ error: `No room with id ${ request.body.id } found` });
-  } else {
-    if (room.activeUsers.find((user) => user.lookup.toString() === request.user.id)) {
-      room.activeUsers = room.activeUsers.filter((user) => user.lookup.toString() !== request.user.id);
-      if (room.permission.indexOf(request.user.id) >= 0) {
-        return room.permission.splice(room.permission.indexOf(request.user.id), 1);
-      }
-      await room.save();
-    }
-
-    response.status(200).json(await Room.populate(room, {
-      path: 'user activeUsers.lookup',
-      select: 'username'
-    }));
-  }
-});
+router.post('/remove/user', passport.authenticate('jwt', { session: false }), setUserOffline);
 
 module.exports = router;
