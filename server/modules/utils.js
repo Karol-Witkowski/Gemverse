3906172b64a99de1de15b9d2db0e1c22f7480a17
Karol Witkowski <karol.witkowski@hotmail.com>
2021-04-21 14:47:04 +0200

    Fix auth 401 error

--- server/modules/utils.js
+++ server/modules/utils.js
@@ -2,11 +2,9 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = {
-  createJwtToken: (model) => {
-    jwt.sign(
-      model.toObject(),
-      process.env.JWT_KEY,
-      { expiresIn: parseInt(process.env.JWT_EXPIRE) }
-    )
-  },
+  createJwtToken: (model) => jwt.sign(
+    model.toObject(),
+    process.env.JWT_KEY,
+    { expiresIn: parseInt(process.env.JWT_EXPIRE) }
+  )
 };
