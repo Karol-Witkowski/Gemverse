2ef4a31673e6f8ca75970784278b9bcf233c70f9
Karol Witkowski <karol.witkowski@hotmail.com>
2021-04-30 20:38:00 +0200

    Setup Login spec
    Basic setup for Login unit tests

--- client/tests/unit/specs/Login.spec.js
+++ client/tests/unit/specs/Login.spec.js
@@ -1,9 +1,28 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, mount } from '@vue/test-utils';
+import Vuetify from 'vuetify';
 import Login from '@/components/authentication/Login.vue';
 
-describe('Login.vue', () => {
-  it('Test - development', () => {
-    const wrapper = shallowMount(Login);
-    expect(wrapper.text()).toMatch('Welcome');
+jest.mock('axios');
+
+describe('CustomCard.vue', () => {
+  const localVue = createLocalVue();
+  let vuetify;
+  let wrapper;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+
+    wrapper = mount(Login, {
+      localVue,
+      stubs: ['router-link', 'router-view'],
+      vuetify,
+      propsData: {
+        message: '',
+      },
+    });
+  });
+
+  it('should have a custom title and match snapshot', () => {
+    expect(wrapper.findAll('button').at(1).text()).toMatch('Clear');
   });
 });
