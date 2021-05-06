a3edd1a74013bf2ebbcd9e83327e1d84de3376a0
Karol Witkowski <karol.witkowski@hotmail.com>
2021-05-06 21:40:37 +0200

    Register implementation test
    Add implementation tests for Register component

--- client/tests/unit/specs/Register.spec.js
+++ client/tests/unit/specs/Register.spec.js
@@ -12,19 +12,26 @@ let wrapper;
 const error = {
   response: {
     data: {
-      password: 'Password error',
-      user: 'Email error',
+      errors: {
+        email: {
+          msg: 'Email error',
+        },
+        username: {
+          msg: 'Username error',
+        },
+      },
     },
   },
 };
 const expectedData = expect.objectContaining({
-  email: 'email value',
-  password: 'password value',
+  email: 'test@email.js',
+  password: 'password',
+  username: 'username',
 });
 const response = {
   data: {
     auth:	true,
-    data: {
+    user: {
       data: 'testData',
     },
     success: true,
@@ -35,7 +42,7 @@ const response = {
 jest.mock('axios');
 vuetify = new Vuetify();
 
-/* describe('Implementation test for Register.vue - successful HTTP post', () => {
+describe('Implementation test for Register.vue - successful HTTP post', () => {
   beforeEach(() => {
     axios.post.mockResolvedValue(response);
 
@@ -49,14 +56,13 @@ vuetify = new Vuetify();
       data() {
         return {
           email: '',
+          emailError: '',
           isFormValid: false,
           password: '',
-          redirectError: this.message
+          username: '',
+          usernameError: '',
         };
       },
-      propsData: {
-        message: ''
-      }
     });
   });
 
@@ -71,101 +77,88 @@ vuetify = new Vuetify();
 
   it('Initializes with correct elements', () => {
     // Test buttons initial state
-    expect(wrapper.findAll('.v-btn').length).toEqual(4);
-    expect(wrapper.findAll('.v-btn').at(0).text()).toMatch('ok');
-    expect(wrapper.findAll('.v-btn').at(1).text()).toMatch('back');
+    expect(wrapper.findAll('.v-btn').length).toEqual(3);
+    expect(wrapper.findAll('.v-btn').at(0).text()).toMatch('back');
+    expect(wrapper.findAll('.v-btn').at(1).text()).toMatch('sign up');
     expect(wrapper.findAll('.v-btn').at(2).text()).toMatch('sign in');
-    expect(wrapper.findAll('.v-btn').at(3).text()).toMatch('sign up');
-    expect(wrapper.findAll('.v-btn').at(2).element.disabled).toBeTruthy();
-
-    // Test auth alert initial state
-    expect(wrapper.findAll('.v-alert').at(0).attributes().class).toContain('whiteSpace');
+    expect(wrapper.findAll('.v-btn').at(1).element.disabled).toBeTruthy();
 
     // Test inputs initial state
-    expect(wrapper.findAll('.v-text-field').length).toEqual(2);
+    expect(wrapper.findAll('.v-text-field').length).toEqual(3);
     expect(wrapper.findAll('input').at(0).text()).toEqual('');
     expect(wrapper.findAll('input').at(1).text()).toEqual('');
+    expect(wrapper.findAll('input').at(2).text()).toEqual('');
 
     // Test validation initial state
-    expect(wrapper.findAll('.v-messages').length).toEqual(2);
+    expect(wrapper.findAll('.v-messages').length).toEqual(3);
     expect(wrapper.findAll('.v-messages').at(0).text()).toEqual('');
     expect(wrapper.findAll('.v-messages').at(1).text()).toEqual('');
+    expect(wrapper.findAll('.v-messages').at(2).text()).toEqual('Minimum length - 6 characters');
     expect(wrapper.vm.isformValid).toBeFalsy();
   });
 
   it('Enables error messages', async () => {
     await wrapper.setData({
-      passwordError: 'Password error',
-      userError: 'Email error'
+      emailError: 'Email error',
+      usernameError: 'Username error',
     });
 
     await Vue.nextTick();
 
-    expect(wrapper.findAll('.v-messages').length).toEqual(2);
-    expect(wrapper.findAll('.v-messages').at(0).text()).toEqual('Email error');
-    expect(wrapper.findAll('.v-messages').at(1).text()).toEqual('Password error');
+    expect(wrapper.findAll('.v-messages').length).toEqual(3);
+    expect(wrapper.findAll('.v-messages').at(0).text()).toEqual('Username error');
+    expect(wrapper.findAll('.v-messages').at(1).text()).toEqual('Email error');
   });
 
-  it('Enables log-in button when email address and password are set', async () => {
+  it('Enables sign up button when user data is set', async () => {
     await wrapper.setData({
-      email: 'email value',
-      password: 'password value'
+      email: 'test@email.js',
+      password: 'password',
+      username: 'username',
     });
 
     await Vue.nextTick();
 
     // Check that the user data is properly set
-    expect(wrapper.findAll('input').at(0).element.value).toEqual('email value');
-    expect(wrapper.findAll('input').at(1).element.value).toEqual('password value');
-    expect(wrapper.vm.email).toBe('email value');
-    expect(wrapper.vm.password).toBe('password value');
+    expect(wrapper.findAll('input').at(0).element.value).toEqual('username');
+    expect(wrapper.findAll('input').at(1).element.value).toEqual('test@email.js');
+    expect(wrapper.findAll('input').at(2).element.value).toEqual('password');
+    expect(wrapper.vm.username).toBe('username');
+    expect(wrapper.vm.email).toBe('test@email.js');
+    expect(wrapper.vm.password).toBe('password');
 
+    await Vue.nextTick();
     // Check if validation pass
     expect(wrapper.vm.isFormValid).toBeTruthy();
 
     // Check that the Register button is active
-    expect(wrapper.findAll('.v-btn').at(2).element.disabled).toBeFalsy();
+    expect(wrapper.findAll('.v-btn').at(1).element.disabled).toBeFalsy();
   });
 
-  it('Fail validation when email and password are not entered', () => {
+ it('Fail validation when user data is not entered', () => {
     expect(wrapper.vm.isFormValid).toBeFalsy();
   });
 
-  it('Fail validation when one field is empty', async () => {
+  it('Fail validation when only one field is entered', async () => {
     await wrapper.setData({
-      email: 'email value'
+      email: 'email value',
     });
 
     await Vue.nextTick();
 
     expect(wrapper.vm.isFormValid).toBeFalsy();
-  });
-
-  it('Enables dismissible auth alert when authentication failed', async () => {
-    await wrapper.setData({
-      redirectError: 'Access denied'
-    });
-
-    // Chek that aut alert is visible
-    expect(wrapper.findAll('.v-alert').at(0).attributes().class).toContain('errorAlert');
-
-    wrapper.vm.hideRedirectError();
-
-    await Vue.nextTick();
-
-    // Chek that aut alert is hidden
-    expect(wrapper.findAll('.v-alert').at(0).attributes().class).toContain('whiteSpace');
   });
 
   it('Should sends post request with correct on form submit', async () => {
     await wrapper.setData({
-      email: 'email value',
-      password: 'password value'
+      email: 'test@email.js',
+      password: 'password',
+      username: 'username',
     });
 
     await Vue.nextTick();
 
-    wrapper.vm.Register();
+    wrapper.vm.createUser();
 
     // Check if post is called
     expect(axios.post).toHaveBeenCalled();
@@ -176,12 +169,12 @@ vuetify = new Vuetify();
     // Check if post is called with correct data
     expect(axios.post).toHaveBeenCalledWith(
       url,
-      expectedData
+      expectedData,
     );
   });
 
   it('Should store user data and auth status after successful Register', async () => {
-    wrapper.vm.Register();
+    wrapper.vm.createUser();
 
     // Check if any action are dispatched
     expect(mockStore.dispatch).toHaveBeenCalled();
@@ -191,36 +184,37 @@ vuetify = new Vuetify();
 
     // Check auth state is dispatched with correct data
     expect(mockStore.dispatch).toHaveBeenNthCalledWith(
-      1, 'remitAuthState', true
+      1, 'remitAuthState', true,
     );
 
     // Check user data is dispatched with correct data
     expect(mockStore.dispatch).toHaveBeenNthCalledWith(
-      2, 'saveUser', response.data.data
+      2, 'saveUser', response.data.user,
     );
   });
-});
+//});
 
-describe('Implementation test for Register.vue - failed HTTP post', () => {
+/* describe('Implementation test for Register.vue - failed HTTP post', () => {
   beforeEach(() => {
     axios.post.mockRejectedValue(error);
 
     wrapper = mount(Register, {
       localVue,
       mocks: {
-        $store: mockStore
+        $store: mockStore,
       },
       stubs: ['router-link', 'router-view'],
       vuetify,
       data() {
         return {
           email: '',
+          emailError: '',
           isFormValid: false,
           password: '',
-          passwordError: '',
-          userError: ''
+          usernameError: '',
+          username: '',
         };
-      }
+      },
     });
   });
 
@@ -234,9 +228,10 @@ describe('Implementation test for Register.vue - failed HTTP post', () => {
 
     await Vue.nextTick();
 
-    expect(wrapper.findAll('.v-messages').length).toEqual(2);
-    expect(wrapper.findAll('.v-messages').at(0).text()).toEqual('Email error');
-    expect(wrapper.findAll('.v-messages').at(1).text()).toEqual('Password error');
+    expect(wrapper.findAll('.v-messages').length).toEqual(3);
+    expect(wrapper.findAll('.v-messages').at(0).text()).toEqual('Username error');
+    expect(wrapper.findAll('.v-messages').at(1).text()).toEqual('Email error');
+    expect(wrapper.findAll('.v-messages').at(2).text()).toEqual('Password error');
   });
 
   it('Does not dispatch data when a failed HTTP post occurs', () => {
@@ -253,7 +248,7 @@ describe('Behavioral test for Register.vue - successful HTTP post', () => {
     wrapper = mount(Register, {
       localVue,
       mocks: {
-        $store: mockStore
+        $store: mockStore,
       },
       stubs: ['router-link', 'router-view'],
       vuetify,
@@ -262,12 +257,9 @@ describe('Behavioral test for Register.vue - successful HTTP post', () => {
           email: '',
           isFormValid: false,
           password: '',
-          redirectError: this.message
+          username: '',
         };
       },
-      propsData: {
-        message: ''
-      }
     });
   });
 
@@ -276,31 +268,16 @@ describe('Behavioral test for Register.vue - successful HTTP post', () => {
     wrapper.destroy();
   });
 
-  it('Dissmiss auth error on click', async () => {
-    await wrapper.setData({
-      redirectError: 'Access denied'
-    });
-
-    // Chek that aut alert is visible
-    expect(wrapper.findAll('.v-alert').at(0).attributes().class).toContain('errorAlert');
-
-    wrapper.findAll('.v-btn').at(0).trigger('click');
-
-    await Vue.nextTick();
-
-    // Chek that aut alert is hidden after click
-    expect(wrapper.findAll('.v-alert').at(0).attributes().class).toContain('whiteSpace');
-  });
-
   it('Should not sends post request when inputs are empty', async () => {
-    wrapper.findAll('.v-btn').at(2).trigger('click');
+    wrapper.findAll('.v-btn').at(1).trigger('click');
 
     expect(axios.post).not.toHaveBeenCalled();
   });
 
   it('Should sends post request with correct on form submit', async () => {
-    await wrapper.findAll('input').at(0).setValue('email value');
-    await wrapper.findAll('input').at(1).setValue('password value');
+    await wrapper.findAll('input').at(0).setValue('username value');
+    await wrapper.findAll('input').at(1).setValue('email value');
+    await wrapper.findAll('input').at(2).setValue('password value');
 
     await Vue.nextTick();
 
@@ -315,12 +292,12 @@ describe('Behavioral test for Register.vue - successful HTTP post', () => {
     // Check if post is called with correct data
     expect(axios.post).toHaveBeenCalledWith(
       url,
-      expectedData
+      expectedData,
     );
   });
 
   it('Should store user data and auth status after successful Register', () => {
-    wrapper.findAll('.v-btn').at(2).trigger('click');
+    wrapper.findAll('.v-btn').at(1).trigger('click');
 
     // Check if any action are dispatched
     expect(mockStore.dispatch).toHaveBeenCalled();
@@ -330,12 +307,12 @@ describe('Behavioral test for Register.vue - successful HTTP post', () => {
 
     // Check auth state is dispatched with correct data
     expect(mockStore.dispatch).toHaveBeenNthCalledWith(
-      1, 'remitAuthState', true
+      1, 'remitAuthState', true,
     );
 
     // Check user data is dispatched with correct data
     expect(mockStore.dispatch).toHaveBeenNthCalledWith(
-      2, 'saveUser', response.data.data
+      2, 'saveUser', response.data.data,
     );
   });
 });
@@ -347,19 +324,20 @@ describe('Behavioral test for Register.vue - failed HTTP post', () => {
     wrapper = mount(Register, {
       localVue,
       mocks: {
-        $store: mockStore
+        $store: mockStore,
       },
       stubs: ['router-link', 'router-view'],
       vuetify,
       data() {
         return {
           email: '',
+          emailError: '',
           isFormValid: false,
           password: '',
-          passwordError: '',
-          userError: ''
+          usernameError: '',
+          username: '',
         };
-      }
+      },
     });
   });
 
@@ -369,23 +347,41 @@ describe('Behavioral test for Register.vue - failed HTTP post', () => {
   });
 
   it('Display error messages on HTTP post failure', async () => {
-    await wrapper.findAll('input').at(0).setValue('email value');
-    await wrapper.findAll('input').at(1).setValue('password value');
+    await wrapper.findAll('input').at(0).setValue('username value');
+    await wrapper.findAll('input').at(1).setValue('email value');
+    await wrapper.findAll('input').at(2).setValue('password value');
 
     await Vue.nextTick();
 
-    await wrapper.findAll('.v-btn').at(2).trigger('click');
+    await wrapper.findAll('.v-btn').at(1).trigger('click');
 
     await Vue.nextTick();
 
-    expect(wrapper.findAll('.v-messages').length).toEqual(2);
-    expect(wrapper.findAll('.v-messages').at(0).text()).toEqual('Email error');
-    expect(wrapper.findAll('.v-messages').at(1).text()).toEqual('Password error');
+    expect(wrapper.findAll('.v-messages').length).toEqual(3);
+    expect(wrapper.findAll('.v-messages').at(0).text()).toEqual('Username error');
+    expect(wrapper.findAll('.v-messages').at(1).text()).toEqual('Email error');
+    expect(wrapper.findAll('.v-messages').at(2).text()).toEqual('?_?_?');
+  });
+
+  it('Hide HTTP post error messages when user enter new data', async () => {
+    await wrapper.setData({
+      emailError: 'Password error',
+      usernameError: 'Email error',
+    });
+
+    await wrapper.findAll('input').at(0).trigger('keyup');
+    await wrapper.findAll('input').at(1).trigger('keyup');
+    await wrapper.findAll('input').at(2).trigger('keyup');
+
+    expect(wrapper.findAll('.v-messages').length).toEqual(3);
+    expect(wrapper.findAll('.v-messages').at(0).text()).toEqual('');
+    expect(wrapper.findAll('.v-messages').at(1).text()).toEqual('');
+    expect(wrapper.findAll('.v-messages').at(2).text()).toEqual('');
   });
 
   it('Does not dispatch data when a failed HTTP post occurs', () => {
-    wrapper.findAll('.v-btn').at(2).trigger('click');
+    wrapper.findAll('.v-btn').at(1).trigger('click');
 
     expect(mockStore.dispatch).not.toHaveBeenCalled();
-  });
-}); */
+  }); */
+});
