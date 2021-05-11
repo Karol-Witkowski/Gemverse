c8faa57a32509d6f8b32614e84d1e42291ea1c55
Karol Witkowski <karol.witkowski@hotmail.com>
2021-05-11 14:57:31 +0200

    Initial profile tests
    Add basic unit tests and snapshot for Profile component

--- client/tests/unit/specs/Profile.spec.js
+++ client/tests/unit/specs/Profile.spec.js
@@ -1,6 +1,6 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import Vuetify from 'vuetify';
-import Navbar from '@/components/layout/Navbar.vue';
+import Profile from '@/components/profile/Profile.vue';
 
 const localVue = createLocalVue();
 const mockRouter = {
@@ -10,19 +10,23 @@ let vuetify;
 let wrapper;
 
 jest.spyOn(window.localStorage.__proto__, 'clear');
-jest.spyOn(window.localStorage.__proto__, 'setItem');
 vuetify = new Vuetify();
 
-describe('Implementation test for Navbar.vue - unauthorized user', () => {
+describe('Implementation test for Profile.vue', () => {
   const mockStore = {
     dispatch: jest.fn(),
     getters: {
-      isAuthorized: false,
+      getUserInfo: {
+        createdDate: '2021-04-20T01:01:22.269Z',
+        email: 'test@mail.js',
+        _id: '321testid',
+        username:"testUser"
+      },
     },
   };
 
   beforeEach(() => {
-    wrapper = mount(Navbar, {
+    wrapper = mount(Profile, {
       localVue,
       mocks: {
         $store: mockStore,
@@ -32,6 +36,14 @@ describe('Implementation test for Navbar.vue - unauthorized user', () => {
         'router-view',
       ],
       vuetify,
+      data() {
+        return {
+          deleteError: '',
+          deleteRoomModal: false,
+          deleteUserModal: false,
+          errors: [],
+        };
+      },
     });
   });
 
@@ -44,7 +56,7 @@ describe('Implementation test for Navbar.vue - unauthorized user', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  it('Initializes with correct elements', () => {
+  /* it('Initializes with correct elements', () => {
     expect(wrapper.findAll('.v-tab').length).toEqual(3);
     expect(wrapper.findAll('.v-tab').at(0).text()).toMatch('home');
     expect(wrapper.findAll('.v-tab').at(1).text()).toMatch('about');
@@ -86,130 +98,11 @@ const mockStore = {
   },
 };
 
-describe('Implementation test for Navbar.vue - authorized user', () => {
+describe('Behavioral test for Profile.vue - authorized user', () => {
   beforeEach(() => {
     localStorage.setItem('authenticationToken', 'testToken');
 
-    wrapper = mount(Navbar, {
-      localVue,
-      mocks: {
-        $router: mockRouter,
-        $store: mockStore,
-      },
-      stubs: [
-        'router-link',
-        'router-view',
-      ],
-      vuetify,
-    });
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-    wrapper.destroy();
-  });
-
-  it('Initializes with correct elements', () => {
-    // Check tabs initial state
-    expect(wrapper.findAll('.v-tab').length).toEqual(6);
-    expect(wrapper.findAll('.v-tab').at(0).text()).toMatch('home');
-    expect(wrapper.findAll('.v-tab').at(1).text()).toMatch('rooms');
-    expect(wrapper.findAll('.v-tab').at(2).text()).toMatch('profile');
-    expect(wrapper.findAll('.v-tab').at(3).text()).toMatch('about');
-    expect(wrapper.findAll('.v-tab').at(4).text()).toMatch('gemverse');
-    expect(wrapper.findAll('.v-tab').at(5).text()).toMatch('logout');
-
-    // Check that mobile menu is rendered
-    expect(wrapper.findAll('button').length).toEqual(1);
-    expect(wrapper.findAll('button').at(0).text()).toMatch('menu');
-  });
-
-  it('Should call dispatch method when created', () => {
-    // Check if any action were dispatched
-    expect(mockStore.dispatch).toHaveBeenCalled();
-
-    // Check if one action was dispatched
-    expect(mockStore.dispatch).toHaveReturnedTimes(1);
-  });
-
-  it('Dispatch correct auth state when created', () => {
-    expect(mockStore.dispatch).toHaveBeenCalledWith(
-      'remitAuthState',
-      true,
-    );
-  });
-
-  it('Dispatch correct data on logout', () => {
-    wrapper.vm.logout();
-
-    // Check if any action were dispatched
-    expect(mockStore.dispatch).toHaveBeenCalled();
-
-    // Check if two actions were dispatched
-    expect(mockStore.dispatch).toHaveReturnedTimes(3);
-
-    // Check auth state was dispatched with correct data
-    expect(mockStore.dispatch).toHaveBeenNthCalledWith(
-      1,
-      'remitAuthState',
-      true,
-    );
-
-    // Check user data was dispatched with correct data
-    expect(mockStore.dispatch).toHaveBeenNthCalledWith(
-      3,
-      'saveUser',
-      '',
-    );
-  });
-
-  it('Add auth state to storage object', () => {
-    wrapper.vm.logout();
-
-    // Check if local storage setter was called
-    expect(localStorage.setItem).toHaveBeenCalled();
-
-    // Check if local storage setter was called once
-    expect(localStorage.setItem).toHaveReturnedTimes(1);
-
-    // Check if local storage setter was called with correct data
-    expect(localStorage.setItem).toHaveBeenCalledWith(
-      'authenticationToken',
-      'testToken',
-    );
-  });
-
-  it('Clear local storage on logout', () => {
-    wrapper.vm.logout();
-
-    // Check if local storage clear method was called
-    expect(localStorage.clear).toHaveBeenCalled();
-
-    // Check if local storage clear method was called once
-    expect(localStorage.clear).toHaveReturnedTimes(1);
-  });
-
-  it('Redirect user to login page on logout', () => {
-    wrapper.vm.logout();
-
-    // Check if router push was called
-    expect(mockRouter.push).toHaveBeenCalled();
-
-    // Check if router push was called one time
-    expect(mockRouter.push).toHaveBeenCalledTimes(1)
-
-    // Check if router push was called with correct object
-    expect(mockRouter.push).toHaveBeenCalledWith({
-      "name": "Login"
-    });
-  });
-});
-
-describe('Behavioral test for Navbar.vue - authorized user', () => {
-  beforeEach(() => {
-    localStorage.setItem('authenticationToken', 'testToken');
-
-    wrapper = mount(Navbar, {
+    wrapper = mount(Profile, {
       localVue,
       mocks: {
         $router: mockRouter,
@@ -291,5 +184,5 @@ describe('Behavioral test for Navbar.vue - authorized user', () => {
     expect(mockRouter.push).toHaveBeenCalledWith({
       "name": "Login"
     });
-  });
+  }); */
 });
