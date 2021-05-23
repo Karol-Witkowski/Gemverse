a8a08f8e2f3d73587c77261fbde0269b83841329
Karol Witkowski <karol.witkowski@hotmail.com>
2021-05-23 16:14:08 +0200

    Update e2e tests - get right elements

--- client/tests/e2e/specs/login.spec.js
+++ client/tests/e2e/specs/login.spec.js
@@ -8,7 +8,7 @@ describe('Login test', () => {
     cy.get('input[name=email]').type('test1@email.tt');
     cy.get('input[name=password]').type('test12');
 
-    cy.contains('sign in').click();
+    cy.get('.v-btn').contains('sign in').click();
 
     // Check if user were redirected to /roomlist
     cy.url().should('include', '/roomlist');
@@ -18,44 +18,44 @@ describe('Login test', () => {
     cy.get('input[name=email]').type('random@email.com');
     cy.get('input[name=password]').type('test12');
 
-    cy.contains('sign in').click();
+    cy.get('.v-btn').contains('sign in').click();
 
     // Check if user are still on the login page
     cy.url().should('include', '/login');
 
     // Check if errors are visible
-    cy.contains('User not found - Try again')
+    cy.get('.v-messages').contains('User not found - Try again')
   });
 
   it('Displays errors when password is wrong', () => {
     cy.get('input[name=email]').type('test1@email.tt');
     cy.get('input[name=password]').type('123456');
 
-    cy.contains('sign in').click();
+    cy.get('.v-btn').contains('sign in').click();
 
     // Check if user are still on the login page
     cy.url().should('include', '/login');
 
     // Check if errors are visible
-    cy.contains('Invalid password');
+    cy.get('.v-messages').contains('Invalid password');
   });
 
   it('Clear input errors', () => {
     cy.get('input[name=email]').type('test1@email.tt');
     cy.get('input[name=password]').type('123456');
 
-    cy.contains('sign in').click();
+    cy.get('.v-btn').contains('sign in').click();
 
     // Check if user are still on the login page
     cy.url().should('include', '/login');
 
     // Check if errors are visible
-    cy.contains('Invalid password');
+    cy.get('.v-messages').contains('Invalid password');
 
     cy.get('input[name=password]').clear();
 
     // Check if errors are not visible after clear
-    cy.contains('Invalid password').should('not.exist');
+    cy.get('.v-messages').contains('Invalid password').should('not.exist');
   });
 
   it('Disable button on empty required inputs', () => {
--- client/tests/e2e/specs/register.spec.js
+++ client/tests/e2e/specs/register.spec.js
@@ -9,7 +9,7 @@ describe('Register test', () => {
     cy.get('input[name=email]').type('tester@email.tt');
     cy.get('input[name=password]').type('test123');
 
-    cy.contains('sign up').click();
+    cy.get('.v-btn').contains('sign up').click();
 
     // Check if user were redirected to /roomlist
     cy.url().should('include', '/roomlist');
@@ -20,13 +20,13 @@ describe('Register test', () => {
     cy.get('input[name=email]').type('random@email.com');
     cy.get('input[name=password]').type('test12');
 
-    cy.contains('sign up').click();
+    cy.get('.v-btn').contains('sign up').click();
 
     // Check if user are still on the register page
     cy.url().should('include', '/register');
 
     // Check if errors are visible
-    cy.contains('Name user1 is already in use');
+    cy.get('.v-messages').contains('Name user1 is already in use');
   });
 
   it('Displays errors when email address is already in use', () => {
@@ -34,13 +34,13 @@ describe('Register test', () => {
     cy.get('input[name=email]').type('test4@email.tt');
     cy.get('input[name=password]').type('test12');
 
-    cy.contains('sign up').click();
+    cy.get('.v-btn').contains('sign up').click();
 
     // Check if user are still on the register page
     cy.url().should('include', '/register');
 
     // Check if errors are visible
-    cy.contains('test4@email.tt address is already in use');
+    cy.get('.v-messages').contains('test4@email.tt address is already in use');
   });
 
   it('Displays errors on wrong email pattern', () => {
@@ -49,7 +49,7 @@ describe('Register test', () => {
     cy.get('input[name=password]').type('test12');
 
     // Check if errors are visible
-    cy.contains('Invalid e-mail');
+    cy.get('.v-messages').contains('Invalid e-mail');
 
     // Check if button is disabled
     cy.contains('sign up').should('be.disabled');
@@ -64,9 +64,9 @@ describe('Register test', () => {
     cy.contains('sign up').should('be.disabled');
 
     // Check if errors are visible
-    cy.contains('Characters range: 3 - 15');
-    cy.contains('E-mail adress must be at least 8 characters long');
-    cy.contains('Password must be at least 6 characters long');
+    cy.get('.v-messages').contains('Characters range: 3 - 15');
+    cy.get('.v-messages').contains('E-mail adress must be at least 8 characters long');
+    cy.get('.v-messages').contains('Password must be at least 6 characters long');
   });
 
   it('Clear input errors', () => {
@@ -75,7 +75,7 @@ describe('Register test', () => {
     cy.get('input[name=password]').type('test12');
 
     // Check if errors are visible
-    cy.contains('Invalid e-mail');
+    cy.get('.v-messages').contains('Invalid e-mail');
 
     // Check if button is disabled
     cy.contains('sign up').should('be.disabled');
@@ -83,7 +83,7 @@ describe('Register test', () => {
     cy.get('input[name=email]').clear();
 
     // Check if errors are not visible after clear
-    cy.contains('Invalid e-mail').should('not.exist');
+    cy.get('.v-messages').contains('Invalid e-mail').should('not.exist');
   });
 
   it('Disable button on empty required input', () => {
